bbb31d4c283b7c20b9ef3ae4b28849f9ce05c23e
vladimir <volt901@icloud.com>
2021-12-16 22:30:40 +0500

    Initial Commit

---
+++ button/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  button
+//
+//  Created by Владимир Ширяев on 16.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ button/buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttonApp.swift
+//  button
+//
+//  Created by Владимир Ширяев on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
